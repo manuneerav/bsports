ad62e00b3fc5b9f1fefec7e18437a25ce2561cf4
iamcaptaain <ayushymnatiwari@gmail.com>
2020-03-25 19:12:07 +0530

    deploy trial

--- bsports/settings.py
+++ bsports/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'svc*ph5nfzz3x-mrb*bywa8qtxg4$#(og@h4!1j8107qm5voc^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
